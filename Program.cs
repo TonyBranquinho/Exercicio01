f5fa9f12ad3eb6af34e3eada1ef539a538b929c9
Tony <antony.branquinho@gmail.com>
2023-12-28 09:44:41 -0300

    bem basico

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp // Exercicio01.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois valores inteiros");
+            int primeiro = int.Parse(Console.ReadLine());
+            int segundo = int.Parse(Console.ReadLine());
+
+            int soma = primeiro + segundo;
+            Console.WriteLine("SOMA = " + soma);
+        }
+    }
+}
